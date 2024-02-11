fd7bd9eed2b3e6caec7ae11d0c52905497a60b2d
Delcio Capolo <delciocapolo096@gmail.com>
2024-02-11 23:16:05 +0100

    working in issue

--- src/controllers/serverController.ts
+++ src/controllers/serverController.ts
@@ -1,6 +1,45 @@
 import { IncomingMessage, ServerResponse } from "node:http";
+import * as URL from "node:url";
+import { debuglog } from "node:util";
 
-export const serverController = (
-  req: IncomingMessage,
-  res: ServerResponse
-) => {};
+const log = debuglog("server");
+
+export const serverController = (req: IncomingMessage, res: ServerResponse) => {
+  const { search, query, pathname, path } = URL.parse(req.url!, true);
+  const urls = [
+    {
+      name: 1,
+      url: "/api",
+    },
+    {
+      name: 2,
+      url: "/api/provider",
+    },
+    {
+      name: 3,
+      url: "/api/buyer",
+    },
+    {
+      name: 4,
+      url: "/api/car",
+    },
+  ];
+
+  const params = [];
+  for (let { url } of urls) {
+    if (url.includes(":")) {
+      params.push({ url, position: url.indexOf(":") });
+    }
+  }
+
+  // if (!urls.includes(req.url!)) {
+  //   return res
+  //     .writeHead(501, { "Content-Type": "application/json" })
+  //     .end({ message: "Route not defined" });
+  // }
+
+  if (search !== null) {
+    if (pathname === "/api/provider" && req.method === "GET") {
+    }
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { createServer } from "node:http";
+import { debuglog } from "node:util";
 import { serverController } from "./controllers/serverController";
+import { AddressInfo } from "node:net";
 
 const server = createServer(serverController);
+const log = debuglog("server");
 
 // if no parameter is provided in list function, it provied automatically
-server.listen();
+server.listen(45181, async () => {
+  const { port } = server.address() as AddressInfo;
+  log(`SERVER IS RUNNING AT http://127.0.0.1:${port}/`);
+});
